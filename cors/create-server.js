e1cac1dbbbce2b2c662ef6bc230cbac64bfa4fa9
Hugo Wride <hugowride@gmail.com>
2020-05-31 23:28:10 +0100

    Add simple CORS tests.
    Boots up two servers and has server 1 serve a HTML page which makes requests to server 1 and server 2 to endpoints which do and don't have CORS setup.

---
+++ cors/create-server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+module.exports = function createServer(port) {
+    // Setup a regular and CORS enabled endpoint.
+    app.get('/regular-endpoint', function (req, res) {
+        res.json({msg: 'This is a non CORS response message.'})
+    })
+    app.get('/cors-enabled-endpoint', cors(), function (req, res) {
+        res.json({msg: 'This is a CORS-enabled response message.'})
+    })
+
+    // Listen.
+    app.listen(port, function () {
+        console.log(`CORS testing server listening on port ${port}`)
+    })
+
+    return app
+}
---
+++ cors/server-2.js
@@ -0,0 +1,1 @@
+require('./create-server')(8081)
