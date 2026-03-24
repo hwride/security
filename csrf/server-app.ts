3bd82dce511ea9564bbf31de77d58569106796a7
Hugo Wride <hugowride@gmail.com>
2026-03-24 20:07:57 +0000

    Install prettier

--- csrf/server-app.ts
+++ csrf/server-app.ts
@@ -1,24 +1,24 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
 
 const fastify = Fastify({
-    logger: {
-        transport: {
-            target: "pino-pretty"
-        }
-    }
-})
+  logger: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+});
 
-fastify.get('/', function (request, reply) {
-    reply.send({ json: 'value' })
-})
+fastify.get("/", function (request, reply) {
+  reply.send({ json: "value" });
+});
 
-fastify.get('/text', function (request, reply) {
-    reply.send('Test response')
-})
+fastify.get("/text", function (request, reply) {
+  reply.send("Test response");
+});
 
 fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-})
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
