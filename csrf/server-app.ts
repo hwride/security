0ec61e57ba97b3cb393fb8a1cb03aafd163396c2
Hugo Wride <hugowride@gmail.com>
2026-03-24 20:19:41 +0000

    Add app server homepage

--- csrf/server-app.ts
+++ csrf/server-app.ts
@@ -7,7 +7,18 @@ const fastify = Fastify({
 });
 
 fastify.get("/", function (request, reply) {
-  reply.send({ json: "value" });
+  reply.type("text/html; charset=utf-8").send(`<html>
+<head>
+  <title>App</title>
+</head>
+<body>
+  <h1>App</h1>
+  <ul>
+    <li><a href="/text">/text</a></li>
+    <li><a href="/json">/json</a></li>
+  </ul>
+</body>
+</html>`);
 });
 
 fastify.get("/text", function (request, reply) {
@@ -16,6 +27,10 @@ fastify.get("/text", function (request, reply) {
 
 fastify.post("/text", function (request, reply) {
   reply.send("POST text response");
+});
+
+fastify.get("/json", function (request, reply) {
+  reply.send({ json: "value" });
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
