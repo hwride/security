007b9355f7061b17e6f0d9406e1f4c973f5f8110
Hugo Wride <hugowride@gmail.com>
2026-03-27 17:44:10 +0000

    fix: refactoring

--- proxy/reverse-proxy-dev-load-balanced.ts
+++ proxy/reverse-proxy-dev-load-balanced.ts
@@ -4,7 +4,11 @@ boot({
   port: 8080,
   backends: {
     "example.com": {
-      servers: [{ url: "http://localhost:3000" }],
+      servers: [
+        { url: "http://localhost:3000" },
+        { url: "http://localhost:3001" },
+      ],
+      loadBalancingPolicy: "random",
     },
     "attacker.com": {
       servers: [{ url: "http://localhost:4000" }],
