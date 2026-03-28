34eebcce9b3b3e3d29c2a2d65e1ce9e7dcdfe882
Hugo Wride <hugowride@gmail.com>
2026-03-28 17:51:39 +0000

    Tweak openssl shell script

--- https/openssl/generate-certificate-test.sh
+++ https/openssl/generate-certificate-test.sh
@@ -12,12 +12,12 @@ mkdir -p certificate-authority
 
 ## Setup Certificate Authority.
 # Generate CA private key.
-echo '--- Generating CA private key ---'
+echo 'Generating CA private key...'
 openssl genrsa -out certificate-authority/private-key.key 2048
 echo ''
 
 # Generate self-signed CA root certificate.
-echo '--- Generating CA root certificate ---'
+echo 'Generating CA root certificate...'
 openssl req -x509 -sha256 -nodes -days 365 \
   -key certificate-authority/private-key.key \
   -out certificate-authority/ca-root.crt \
@@ -26,20 +26,20 @@ openssl req -x509 -sha256 -nodes -days 365 \
 ## Setup Server.
 # Generate server private key.
 echo ''
-echo '--- Generating server private key ---'
+echo 'Generating server private key...'
 openssl genrsa -out server/private-key.key 2048
 
 ## Create and submit Certificate Signing Request.
 # Generate CSR using server private key.
 echo ''
-echo '--- Generating certificate signing request ---'
+echo 'Generating certificate signing request...'
 openssl req -new \
   -key server/private-key.key \
   -out server/server.csr \
   -subj "/C=UK/ST=London/L=London/O=SSL Test Org/OU=IT/CN=localhost"
 
 echo ''
-echo '--- CA creating signed certificate from CSR ---'
+echo 'CA creating signed certificate from CSR...'
 # Have CA produce a certificate from the CSR.
 openssl x509 -req \
   -in server/server.csr \
@@ -62,11 +62,15 @@ echo 'View CA signed certificated: openssl x509 -in server/signed-cert.crt -text
 echo ''
 
 echo 'Server public keys'
+echo '------------------'
+echo 'All these places shared the same server public key.'
 echo 'From private key: openssl rsa -in server/private-key.key -pubout'
 echo "From CSR: openssl req -in server/server.csr -noout -pubkey"
 echo "From CA signed certificated: openssl x509 -in server/signed-cert.crt -noout -pubkey"
 echo ''
 
 echo 'CA public keys'
+echo '--------------'
+echo 'All these places shared the same CA public key.'
 echo 'From private key: openssl rsa -in certificate-authority/private-key.key -pubout'
 echo 'From CA root certificate: openssl x509 -in certificate-authority/ca-root.crt -noout -pubkey'
