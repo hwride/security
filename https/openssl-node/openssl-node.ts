71dbe9931774dc7b39f9346f7df9b8cd81727a61
Hugo Wride <hugowride@gmail.com>
2026-03-29 13:10:58 +0100

    Add v1 of openssl-node

--- https/openssl-node/openssl-node.ts
+++ https/openssl-node/openssl-node.ts
@@ -1,1 +1,29 @@
-console.log("Hello");
+import { execFile as execFileCallback } from "node:child_process";
+import { promisify } from "node:util";
+
+const execFile = promisify(execFileCallback);
+
+export type OpenSSLResult = {
+  command: string;
+  args: string[];
+  stdout: string;
+  stderr: string;
+  /** This is just for nice logging. */
+  commandText: string;
+};
+
+export async function openssl(
+  command: string,
+  args: string[],
+): Promise<OpenSSLResult> {
+  const fullArgs = [command, ...args];
+  const { stdout, stderr } = await execFile("openssl", fullArgs);
+
+  return {
+    command,
+    args,
+    stdout,
+    stderr,
+    commandText: ["openssl", ...fullArgs].join(" "),
+  };
+}
