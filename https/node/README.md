09a8d7a3997cc1131282f84a9d599fb480dc7558
Hugo Wride <hugowride@gmail.com>
2026-03-30 22:44:05 +0100

    Add standalone script for booting https server

---
+++ https/node/README.md
@@ -0,0 +1,6 @@
+# Node HTTPS
+## Run the test server
+1. Run `npm run generate-certs` to generate some test certificates.
+1. Run `npm run install-root-ca` to install the test root CA certificate into the trust store.
+1. Run `npm run start` to boot the test HTTPS server.
+1. Load https://localhost:8080 in the browser and see a valid HTTPS connection.
---
+++ https/node/https-server.ts
@@ -0,0 +1,64 @@
+import { existsSync, readFileSync } from "node:fs";
+import * as https from "node:https";
+import { join, resolve } from "node:path";
+
+const serverPort = 8080;
+
+main().catch(handleFatalError);
+
+async function main() {
+  const buildDir = resolve(process.cwd(), "build");
+  const serverPrivateKeyPath = join(
+    buildDir,
+    "server",
+    "server-private-key.key",
+  );
+  const serverSignedCertPath = join(buildDir, "server", "signed-cert.crt");
+
+  assertFileExists(
+    serverPrivateKeyPath,
+    "Server private key not found. Generate certificates into ./build first.",
+  );
+  assertFileExists(
+    serverSignedCertPath,
+    "Server certificate not found. Generate certificates into ./build first.",
+  );
+
+  const server = https.createServer(
+    {
+      cert: readFileSync(serverSignedCertPath),
+      key: readFileSync(serverPrivateKeyPath),
+    },
+    function handleRequest(_request, response) {
+      response.writeHead(200);
+      response.end("HTTPS response");
+    },
+  );
+
+  await new Promise<void>((resolve) => {
+    server.listen(serverPort, resolve);
+  });
+
+  console.log(`HTTPS server listening at https://localhost:${serverPort}`);
+  console.log(`Using certificate: ${serverSignedCertPath}`);
+  console.log(`Using private key: ${serverPrivateKeyPath}`);
+
+  process.on("SIGINT", () => {
+    server.close(() => {
+      process.exit(0);
+    });
+  });
+}
+
+function assertFileExists(filePath: string, errorMessage: string) {
+  if (existsSync(filePath)) {
+    return;
+  }
+
+  throw new Error(`${errorMessage} Missing file: ${filePath}`);
+}
+
+function handleFatalError(error: unknown) {
+  console.error(error);
+  process.exitCode = 1;
+}
