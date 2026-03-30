6e894563d4bdcdf5b422184d2db07366d3429f7e
Hugo Wride <hugowride@gmail.com>
2026-03-30 21:56:43 +0100

    Add HTTPS failure when certificate signature is not trusted

--- https/node/https.test.ts
+++ https/node/https.test.ts
@@ -4,7 +4,7 @@ import { resolve } from "node:path";
 import { expect, test } from "vitest";
 import { generateCertificateTest } from "../openssl-node-https-certs-demo/generate-certificate-test.ts";
 
-test("https", async () => {
+test("successful request", async () => {
   expect.hasAssertions();
 
   /* Create:
@@ -67,6 +67,73 @@ test("https", async () => {
     );
 
     request.on("error", reject);
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    server.close((error) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      resolve();
+    });
+  });
+});
+
+test("server certificate is not signed by a trusted root certificate", async () => {
+  expect.hasAssertions();
+
+  /* Create:
+      1) A self-signed certificate authority certificate.
+      2) A server certificate, signed by our custom CA. And a server private key.
+         These will be used to boot our HTTPS server.
+   */
+  const { serverPrivateKeyPath, serverSignedCertPath } =
+    await generateCertificateTest(resolve(process.cwd(), "build"));
+
+  // Boot our HTTPS server.
+  const server = https.createServer(
+    {
+      // Pass in the server's HTTPS certificate.
+      // The certificate contains the server's public key and other identifying information such as domain name.
+      // It is signed by our custom certificate authority, which we will tell the client to trust. So the client
+      // can later verify the signature on the server certificate is trusted.
+      cert: readFileSync(serverSignedCertPath),
+
+      // Pass in the server's private key.
+      // During the TLS handshake, the server will prove it is the legitimate holder of the certificate by proving
+      // it owns the private key corresponding to the public key embedded in the certificate.
+      // It does this by signing fresh handshake data with its private key, which the client can then
+      // verify using the public key embedded in the certificate.
+      key: readFileSync(serverPrivateKeyPath),
+    },
+    function handleRequest(_request, response) {
+      response.writeHead(200);
+      response.end("HTTPS response");
+    },
+  );
+
+  await new Promise<void>((resolve) => {
+    server.listen(8080, resolve);
+  });
+
+  // Check the HTTPS request fails because the server certificate is not signed by a trusted root certificate.
+  await expect(
+    new Promise<void>((resolve, reject) => {
+      const request = https.get(
+        "https://localhost:8080",
+        // Don't include our custom CA cert here, to cause the HTTPS request to fail due to an untrusted signature.
+        () => {
+          resolve();
+        },
+      );
+
+      request.on("error", reject);
+    }),
+  ).rejects.toMatchObject({
+    code: "UNABLE_TO_VERIFY_LEAF_SIGNATURE",
+    message: expect.stringContaining("unable to verify the first certificate"),
   });
 
   await new Promise<void>((resolve, reject) => {
