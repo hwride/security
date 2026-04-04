c2a982b5f8f956dde1f221a45110274518c55848
Hugo Wride <hugowride@gmail.com>
2026-04-04 12:22:16 +0100

    Make CSRF proxy HTTPS

--- csrf/proxy.ts
+++ csrf/proxy.ts
@@ -1,7 +1,13 @@
 import { boot } from "../proxy/reverse-proxy.ts";
+import { readFileSync } from "node:fs";
 
 boot({
-  port: 80,
+  port: 443,
+  proxyProtocol: "https",
+  tls: {
+    key: readFileSync("proxy-cert/private-key.key"),
+    cert: readFileSync("proxy-cert/cert.crt"),
+  },
   backends: {
     "example.com": {
       servers: [{ url: "http://localhost:3000" }],
